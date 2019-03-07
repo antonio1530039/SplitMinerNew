48f0253bddc7cf9a151dca116d4c695f9c773dbc
1530039@upv.edu.mx <1530039@upv.edu.mx>
2019-03-06 22:38:02 -0600

    @

--- src/Controlador/RepairOutliers.java
+++ src/Controlador/RepairOutliers.java
@@ -15,35 +15,86 @@ public class RepairOutliers {
         //Dado tc (porcentaje para considerar un contexto como significante) calcular el numero de trazas en el que debe existir el contexto
         double minimumTraces = tracesList.size() * tc;
         
-        HashMap<ArrayList<LinkedHashSet<Character>>, SignificantContext> significantContexts = new HashMap<>();
+        HashMap<ArrayList<ArrayList<Character>>, SignificantContext> significantContexts = new HashMap<>();
         
         for(Map.Entry<Integer, ArrayList<Character>> entry : tracesList.entrySet()){
-            
             ArrayList<Character> trace = entry.getValue();
-            trace.add(0, 'O');
-            trace.add('O');
-            for(Character t : trace){
-                ArrayList<Character> secuence = new ArrayList<>();
-                secuence.add(t);
-                ArrayList<LinkedHashSet<Character>> con = context(secuence, trace, l , r);
-                if(significantContexts.containsKey(con)){
-                    SignificantContext sc = significantContexts.get(con);
-                    sc.Frecuency++;
-                    sc.ProbableSubsequences.add(t);
-                }else{
-                    significantContexts.put(con, new SignificantContext());
+            System.out.println("Trace: " + trace.toString());
+            ArrayList<Integer> secuence = new ArrayList<>();
+            for(int i = 0; i < trace.size(); i++){
+                secuence.add(i);
+                if(secuence.size() == K){
+                    ArrayList<ArrayList<Character>> con = context(secuence, trace, l , r);
+                    ArrayList<ArrayList<Character>> con2 = new ArrayList<>();
+                    
+                    ArrayList<Character> realSecuence = new ArrayList<>();
+                    for(Integer index : secuence){
+                        realSecuence.add(trace.get(index));
+                    }
+                    con2.add(con.get(0));
+                    con2.add((ArrayList)realSecuence.clone());
+                    
+                    if(significantContexts.containsKey(con)){
+                        SignificantContext sc = significantContexts.get(con);
+                        sc.Frecuency++;
+                        LinkedHashSet lhmrealSecuence = new LinkedHashSet<>();
+                        lhmrealSecuence.addAll(realSecuence);
+                        
+                        if(sc.ProbableSubsequences.containsKey(lhmrealSecuence)){
+                            Integer freq = sc.ProbableSubsequences.get(lhmrealSecuence);
+                            freq++;
+                            sc.ProbableSubsequences.put(lhmrealSecuence, freq);
+                        }else{
+                            sc.ProbableSubsequences.put(lhmrealSecuence, 1);
+                        }
+                    }else{
+                        SignificantContext sc = new SignificantContext();
+                        LinkedHashSet lhmrealSecuence = new LinkedHashSet<>();
+                        lhmrealSecuence.addAll(realSecuence);
+                        sc.ProbableSubsequences.put(lhmrealSecuence, 1);
+                        significantContexts.put(con, sc);
+                    }
+                    
+                    
+                    
+                    if(significantContexts.containsKey(con2)){
+                        SignificantContext sc = significantContexts.get(con2);
+                        sc.Frecuency++;
+                        LinkedHashSet lhmrealSecuence = new LinkedHashSet<>();
+                        lhmrealSecuence.add('O');
+                        if(sc.ProbableSubsequences.containsKey(lhmrealSecuence)){
+                            Integer freq = sc.ProbableSubsequences.get(lhmrealSecuence);
+                            freq++;
+                            sc.ProbableSubsequences.put(lhmrealSecuence, freq);
+                            
+                        }else{
+                            sc.ProbableSubsequences.put(lhmrealSecuence, 1);
+                        }
+                    }else{
+                        SignificantContext sc = new SignificantContext();
+                        LinkedHashSet lhmrealSecuence = new LinkedHashSet<>();
+                        lhmrealSecuence.addAll(realSecuence);
+                        sc.ProbableSubsequences.put(lhmrealSecuence, 1);
+                        significantContexts.put(con2, sc);
+                    }
+                    secuence.clear();
                 }
             }
         }
         System.out.println("SignificantContextes");
-        for(Map.Entry<ArrayList<LinkedHashSet<Character>>, SignificantContext> entry : significantContexts.entrySet()){
+        for(Map.Entry<ArrayList<ArrayList<Character>>, SignificantContext> entry : significantContexts.entrySet()){
             SignificantContext sc = entry.getValue();
-            //if(sc.Frecuency >= minimumTraces){
+            if(sc.Frecuency >= minimumTraces){
                 System.out.println("SignificantContext: " + entry.getKey().toString());
                 System.out.println("Frecuency: " + entry.getValue().Frecuency);
-                System.out.println("ProbableSubsequences: " + entry.getValue().ProbableSubsequences);
+                System.out.println("ProbableSubsequences: ");
+                
+                for(Map.Entry<LinkedHashSet<Character>, Integer> entry2: entry.getValue().ProbableSubsequences.entrySet()){
+                    System.out.println("\tSequence: " + entry2.getKey());
+                    System.out.println("\tfreq: " + entry2.getValue());
+                }
                 System.out.println("");
-            //}
+            }
             
         }
         System.out.println("minimumTraces for SC: " + minimumTraces);
@@ -51,15 +102,15 @@ public class RepairOutliers {
     
     
     
-    public static ArrayList<LinkedHashSet<Character>> context(ArrayList<Character> secuence, ArrayList<Character> trace, int l, int r ){
+    public static ArrayList<ArrayList<Character>> context(ArrayList<Integer> secuence, ArrayList<Character> trace, int l, int r ){
         //Dada una secuencia de tareas y una traza; encontrar el contexto de la secuencia...
         //...según el número de vecinos requeridos a la izquierda y a la derecha indicados en los parámetros 'l' y 'r' respectivamente
-        LinkedHashSet<Character> leftNeighbour = new LinkedHashSet<>();
-        LinkedHashSet<Character> rightNeighbour = new LinkedHashSet<>();
-        ArrayList<LinkedHashSet<Character>> context = new ArrayList<>();
+        ArrayList<Character> leftNeighbour = new ArrayList<>();
+        ArrayList<Character> rightNeighbour = new ArrayList<>();
+        ArrayList<ArrayList<Character>> context = new ArrayList<>();
         
-        int subLeft = trace.indexOf(secuence.get(0)); //posición de la traza del primer elemento de la secuencia
-        int subRight = trace.indexOf(secuence.get(secuence.size()-1) ); //indice de la traza del ultimo elemento en la secuencia
+        int subLeft = secuence.get(0); //posición de la traza del primer elemento de la secuencia
+        int subRight = secuence.get(secuence.size()-1); //indice de la traza del ultimo elemento en la secuencia
         
         //verificamos que la posición en la traza del primer elemento en la secuencia menos el número de elementos requeridos hacia la izquierda desde sea mayor a 0
         if( (subLeft - l) >= 0){ 
--- src/Modelo/SignificantContext.java
+++ src/Modelo/SignificantContext.java
@@ -1,13 +1,16 @@
 
 package Modelo;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 
 
 public class SignificantContext {
     
     public int Frecuency = 1;
-    public LinkedHashSet<Character> ProbableSubsequences = new LinkedHashSet<>();
+    
+    public LinkedHashMap<LinkedHashSet<Character>, Integer> ProbableSubsequences = new LinkedHashMap<>();
+    //public LinkedHashSet<LinkedHashSet<Character>> ProbableSubsequences = new LinkedHashSet<>();
     
     
 }
